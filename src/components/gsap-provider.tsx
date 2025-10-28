401e3df5094528a93f0cc3f7bfeec068547dca5c
Shady Ossama <shadyosama658@gmail.com>
2025-10-28 22:19:15 +0000

    still not working

--- src/components/gsap-provider.tsx
+++ src/components/gsap-provider.tsx
@@ -4,37 +4,15 @@
 import { useEffect } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { ScrollSmoother } from 'gsap/ScrollSmoother';
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-import { useIsMobile } from '@/hooks/use-mobile';
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother, ScrollToPlugin);
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 export function GsapProvider({ children }: { children: React.ReactNode }) {
-  const isMobile = useIsMobile();
-
   useEffect(() => {
-    // Only run ScrollSmoother on non-mobile devices for better performance
-    if (isMobile === false) {
-      const smoother = ScrollSmoother.create({
-        smooth: 1,
-        effects: true,
-        smoothTouch: 0.1,
-      });
+    // Refresh ScrollTrigger on mount to ensure calculations are correct.
+    ScrollTrigger.refresh();
+  }, []);
 
-      return () => {
-        // Kill the smoother instance on component unmount
-        smoother.kill();
-      };
-    } else {
-      // On mobile, just refresh ScrollTrigger
-      ScrollTrigger.refresh();
-    }
-  }, [isMobile]);
-
-  return (
-    <div id="smooth-wrapper">
-      <div id="smooth-content">{children}</div>
-    </div>
-  );
+  return <>{children}</>;
 }
